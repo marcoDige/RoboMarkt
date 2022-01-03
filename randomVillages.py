9db1421d800155dc8930856742627ee1e012ee01
orsoFra <francesco.corso@mail.polimi.it>
2022-01-03 19:49:19 +0100

    fixed a ;

--- randomVillages.py
+++ randomVillages.py
@@ -1,12 +1,12 @@
 import random
 
-number = random.randint(100,500)
+number = random.randint(500,500)
 rang = random.randint(1,50)
 Vc = random.randint(1,4)
 Fc = random.randint(1,100)
 capacity = random.randint(5,20)
-maxX = 50
-maxY = 50
+maxX = 200
+maxY = 200
 maxC = 20
 
 with open('minimart-'+str(number)+'.dat', 'w') as f:
@@ -14,7 +14,7 @@ with open('minimart-'+str(number)+'.dat', 'w') as f:
     f.write('param range :=    '+str(rang)+'; \n')
     f.write('param Vc :=    '+str(Vc)+'; \n')
     f.write('param Fc :=	'+str(Fc)+'	; \n')			
-    f.write('param capacity :='+str(capacity)+"\n")
+    f.write('param capacity :='+str(capacity)+";\n")
     f.write('param:	Cx  Cy	Dc	usable	:= \n')
     for i in range(1,number+1):
         x = random.randint(1,maxX)
